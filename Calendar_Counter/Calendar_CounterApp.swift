527c79de9ac19464589e25da5cd2ad05f3f7ff10
Alex Kolch <alexkolch.ios@gmail.com>
2025-05-06 16:41:17 +0300

    Initial Commit

---
+++ Calendar_Counter/Calendar_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_CounterApp.swift
+//  Calendar_Counter
+//
+//  Created by Алексей Колыченков on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calendar_Counter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calendar_Counter
+//
+//  Created by Алексей Колыченков on 06.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
