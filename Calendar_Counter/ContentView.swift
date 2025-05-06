6839a85d460bb7b4c8f9e08d0ecc4289d10fa809
Alex Kolch <alexkolch.ios@gmail.com>
2025-05-06 17:19:53 +0300

    Start Screen

--- Calendar_Counter/ContentView.swift
+++ Calendar_Counter/ContentView.swift
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 0.0) {
+                Text("6.05")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundStyle(Color(hex: "727272"))
+                
+                HStack {
+                    Text("today")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 40, weight: .bold))
+                    Spacer()
+                    chevron
+                    Spacer()
+                    Text("3 days")
+                        .foregroundStyle(Color(hex: "00A907"))
+                        .font(.system(size: 40, weight: .bold))
+                }
+              
+            }
+            Spacer()
+            Text("Start in three days")
+                .font(.system(size: 40, weight: .bold))
+                .foregroundStyle(.white)
+            Spacer()
         }
-        .padding()
+        .padding(.horizontal, 37)
+        .padding(.vertical, 50)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color(hex: "2D2D2D"))
+    }
+}
+
+private extension ContentView {
+    var chevron: some View {
+        HStack {
+            Rectangle()
+                .frame(width: 50, height: 2)
+            Image(systemName: "chevron.right")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 10)
+                .offset(x: -10)
+        }
+        .foregroundStyle(.white)
     }
 }
 
