2f7014a7d21f4284d0a3aa9b591e5de27e7bce57
Alex Kolch <alexkolch.ios@gmail.com>
2025-05-06 18:38:05 +0300

    DateView done

--- Calendar_Counter/ContentView.swift
+++ Calendar_Counter/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowDateView = false
+    
     var body: some View {
         VStack(alignment: .leading) {
             VStack(alignment: .leading, spacing: 0.0) {
@@ -25,6 +27,9 @@ struct ContentView: View {
                     Text("3 days")
                         .foregroundStyle(Color(hex: "00A907"))
                         .font(.system(size: 40, weight: .bold))
+                        .onTapGesture {
+                            isShowDateView.toggle()
+                        }
                 }
               
             }
@@ -38,6 +43,10 @@ struct ContentView: View {
         .padding(.vertical, 50)
         .frame(maxWidth: .infinity, alignment: .leading)
         .background(Color(hex: "2D2D2D"))
+        .sheet(isPresented: $isShowDateView) {
+            DateView()
+                .presentationDetents([.height(600)])
+        }
     }
 }
 
@@ -58,4 +67,46 @@ private extension ContentView {
 
 #Preview {
     ContentView()
+}
+
+struct DateView: View {
+    @State var selectedDate: Date = Date()
+    @State var text: String = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack(spacing: 26.0) {
+            DatePicker("", selection: $selectedDate, displayedComponents: .date)
+                .datePickerStyle(.graphical)
+                .padding()
+                .background(Color(hex: "1C1C1E"))
+                .clipShape(.rect(cornerRadius: 20))
+                .environment(\.colorScheme, .dark)
+            
+            TextField("Date text", text: $text)
+                .padding(30)
+                .foregroundStyle(.white)
+                .font(.system(size: 20, weight: .bold))
+                .background(Color(hex: "1C1C1E"))
+                .clipShape(.rect(cornerRadius: 20))
+            
+            Button {
+                dismiss()
+            } label: {
+                Text("Set")
+                    .padding(.vertical, 18)
+                    .frame(maxWidth: .infinity)
+                    .font(.system(size: 25, weight: .bold))
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .clipShape(.rect(cornerRadius: 20))
+            }
+
+        }
+        .padding(.horizontal, 20)
+        .padding(.top, 30)
+        .frame(maxHeight: .infinity, alignment: .top)
+        .background(.black)
+    }
+        
 }
